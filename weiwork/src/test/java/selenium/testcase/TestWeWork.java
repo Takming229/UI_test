5aa9be1b8425d0e3265852d69f87f7cb915676d9
Takming229 <2294706129@qq.com>
2021-02-19 22:10:16 +0800

    代码重构（让App类、ContactPage类继承BasePage基础类）

--- weiwork/src/test/java/selenium/testcase/TestWeWork.java
+++ weiwork/src/test/java/selenium/testcase/TestWeWork.java
@@ -2,11 +2,7 @@ package selenium.testcase;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Cookie;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import selenium.Page.App;
 
 import java.util.concurrent.TimeUnit;
